3f9a52506317e09d6d178b11d4bebaa184b2328a
CerealZBasilisk <93325934+CerealZBasilisk@users.noreply.github.com>
2025-01-28 08:51:35 -0500

    added word counting and character counting to main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+from collections import Counter
+
+
 def read_book(book_path):
     file_contents = None
     with open(book_path) as f:
         file_contents = f.read()
-    print(file_contents)
+    # print(file_contents)
+    return file_contents
 
+
+def word_count(book):
+    count = len(book.split())
+    # print(count)
+    return count
+
+def character_counts(book):
+    character_count = Counter(book.lower())
+    return character_count
 
 if __name__ == "__main__":
-    read_book("books/frankenstein.txt")
+
+    book = read_book("books/frankenstein.txt")
+    count = word_count(book)
+    characters_counts = character_counts(book)
+    print(characters_counts)
